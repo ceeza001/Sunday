76882f110df114334947d4bf4e5a1010e86ae43f
ceeza001 <mesalokubor@gmail.com>
2024-09-01 09:15:55 +0000

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        tsconfigPath: './tsconfig.json',
+        ignoreBuildErrors: true, // This will ignore build errors
+      },
+    }),
+  ],
   assetsInclude: ['**/*.glb'],
   server: {
     host: '0.0.0.0',
